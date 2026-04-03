423325e686b51df6577d04e91c447aa787bc5bea
John Hurrell <jhurrell@gmail.com>
2026-04-03 15:47:04 -0400

    Populating the admin screen.

--- src/lib/server-functions.ts
+++ src/lib/server-functions.ts
@@ -1,11 +1,31 @@
+import { db } from "@/db";
+import { account, session } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { createServerFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
+import { gt } from "drizzle-orm";
 
 export const getSessionFn = createServerFn({ method: "GET" }).handler(
   async () => {
     const headers = getRequestHeaders();
-    const session = await auth.api.getSession({ headers });
-    return session ?? undefined;
+    const currentSession = await auth.api.getSession({ headers });
+    return currentSession ?? undefined;
+  },
+);
+
+export const getUserSessionsFn = createServerFn({ method: "GET" }).handler(
+  async () => {
+    const activeSessions = await db
+      .select()
+      .from(session)
+      .where(gt(session.expiresAt, new Date()));
+    return activeSessions;
+  },
+);
+
+export const getUserAccountsFn = createServerFn({ method: "GET" }).handler(
+  async () => {
+    const accounts = await db.select().from(account);
+    return accounts;
   },
 );
--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -1,4 +1,20 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { authClient } from "@/lib/auth-client";
+import { getUserAccountsFn, getUserSessionsFn } from "@/lib/server-functions";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/admin/")({
@@ -7,14 +23,28 @@ export const Route = createFileRoute("/admin/")({
     // const results = await auth.api.listUsers();
 
     const { data, error } = await authClient.admin.listUsers({
-      query: { sortBy: "name", sortDirection: "desc" },
+      query: { sortBy: "name", sortDirection: "asc" },
     });
 
     if (error) {
       return [];
     }
 
-    return data.users;
+    const userSessions = await getUserSessionsFn();
+    const userAccounts = await getUserAccountsFn();
+
+    const users = data.users.map((user) => {
+      const activeSessions = userSessions.filter(
+        (session) => session.userId === user.id,
+      );
+
+      const activeAccounts = userAccounts.filter(
+        (account) => account.userId === user.id,
+      );
+      return { ...user, activeSessions, activeAccounts };
+    });
+
+    return users;
   },
 });
 
@@ -22,22 +52,63 @@ function RouteComponent() {
   const users = Route.useLoaderData();
   return (
     <div>
-      <h1>User Admimnistration</h1>
-      <ul>
-        {users.map((user, idx) => (
-          <li key={idx}>
-            User[{idx}]: {user.name} ({user.email}) - {user.role ?? "visitor"}{" "}
-            {user.image && (
-              <img
-                src={user.image}
-                alt={`${user.name}'s profile picture`}
-                width={32}
-                height={32}
-              />
-            )}
-          </li>
-        ))}
-      </ul>
+      <div className="mx-auto container my-6 px-4">
+        <Card>
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2">
+              Users ({users.length})
+            </CardTitle>
+            <CardDescription>
+              Manage user accounts, roles, and permissions
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="rounded-md border">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Name</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Created</TableHead>
+                    <TableHead>Role</TableHead>
+                    <TableHead>Active Sessions</TableHead>
+                    <TableHead>From Accounts</TableHead>
+                    <TableHead className="w-25">Actions</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {users.map((user) => (
+                    <TableRow key={user.id}>
+                      <TableCell>{user.name}</TableCell>
+                      <TableCell>{user.email}</TableCell>
+                      <TableCell>{user.createdAt.toISOString()}</TableCell>
+                      <TableCell>{user.role ?? "visitor"}</TableCell>
+                      <TableCell>
+                        <ul>
+                          {user.activeSessions.map((session) => (
+                            <li key={session.id}>
+                              Session ID: {session.id}
+                              <br></br>Expires At:{" "}
+                              {session.expiresAt.toISOString()}
+                            </li>
+                          ))}
+                        </ul>
+                      </TableCell>
+                      <TableCell>
+                        <ul>
+                          {user.activeAccounts.map((account) => (
+                            <li key={account.id}>* {account.providerId}</li>
+                          ))}
+                        </ul>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
