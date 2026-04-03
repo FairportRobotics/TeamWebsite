3ede45c0b9579478d3b4a34eabdf272ccf295446
John Hurrell <jhurrell@gmail.com>
2026-04-03 13:16:17 -0400

    Skeleton to list users.

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -2,14 +2,20 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/admin/")({
   component: RouteComponent,
+  loader: async () => {
+    return [];
+  },
 });
 
 function RouteComponent() {
+  const users = Route.useLoaderData();
   return (
     <div>
       <h1>User Admimnistration</h1>
       <ul>
-        <li></li>
+        {users.map((user, idx) => (
+          <li key={idx}>User[{idx}]</li>
+        ))}
       </ul>
     </div>
   );
