293a5006c774fc4096c6387ad29d24ddf4f0f6f8
John Hurrell <jhurrell@gmail.com>
2026-04-04 09:12:38 -0400

    Added a comment to the API route to provide some context as to its function.

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -1,6 +1,8 @@
 import { auth } from "@/lib/auth";
 import { createFileRoute } from "@tanstack/react-router";
 
+// This is used by Better-Auth to handle all auth related API routes.
+// It should not be modified unless you know what you're doing.
 export const Route = createFileRoute("/api/auth/$")({
   server: {
     handlers: {
