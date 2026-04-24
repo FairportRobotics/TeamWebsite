2b9c5e8798cd3dccc2c9b39cf155ab0b094c9ad5
John Hurrell <jhurrell@gmail.com>
2026-04-24 08:29:25 -0400

    Casing of files kept changing. Had to configure git to stop doing this.

---
+++ src/components/header.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+// prettier-ignore
+import { authClient } from "@/lib/auth-client";
+import { Link, useNavigate } from "@tanstack/react-router";
+import type { User } from "better-auth";
+import { ImpersonateButton } from "./impersonate-button";
+import { Button } from "./ui/button";
+
+export interface NavUserProps {
+  user?: User | undefined;
+}
+
+export default function Header() {
+  const navigate = useNavigate();
+  const { data: session, isPending } = authClient.useSession();
+
+  async function handleSignOut() {
+    await authClient.signOut();
+    navigate({ to: "/" });
+  }
+
+  return (
+    <header className="sticky top-0 z-50 bg-(--color-accent) px-4 backdrop-blur-lg">
+      <nav className="flex flex-wrap items-center justify-between gap-x-3 gap-y-2 py-3 sm:py-4">
+        <section className="flex items-center justify-center gap-3">
+          <Link to="/">Home</Link>
+          <Link to="/team">Team</Link>
+          <Link to="/games">Games</Link>
+          <Link to="/calendar">Calendar</Link>
+          <Link to="/sponsors">Sponsors</Link>
+          <Link to="/admin">Admin</Link>
+        </section>
+        <section className="flex items-center justify-center gap-3">
+          {!isPending && (
+            <>
+              {session ? (
+                <>
+                  <ImpersonateButton />
+                  <Button variant="destructive" onClick={() => handleSignOut()}>
+                    Sign Out
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button variant="default">
+                    <Link to="/auth/signin">Sign In</Link>
+                  </Button>
+                </>
+              )}
+            </>
+          )}
+        </section>
+      </nav>
+    </header>
+  );
+}
