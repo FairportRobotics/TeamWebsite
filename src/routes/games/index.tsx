89599b8024ba242d3e23e7f388b62ec9dc6cd47b
John Hurrell <jhurrell@gmail.com>
2026-04-24 09:02:34 -0400

    Games are a little better but not great.

--- src/routes/games/index.tsx
+++ src/routes/games/index.tsx
@@ -1,15 +1,5 @@
-import {
-  PageDescription,
-  PageHeader,
-  PageTitle,
-} from "@/components/page-header";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { PageDescription, PageHeader, PageTitle } from "@/components/page-header";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { getGameYearsFn } from "@/lib/fn/games";
 import { createFileRoute, Link } from "@tanstack/react-router";
 
@@ -31,27 +21,29 @@ function RouteComponent() {
           FIRST <span className="text-(--color-destructive)">games</span>
         </PageTitle>
         <PageDescription>
-          This is where we can list each game year and include the robot name
-          and any other awards or accolades we achieved.
+          This is where we can list each game year and include the robot name and any other awards
+          or accolades we achieved.
         </PageDescription>
       </PageHeader>
 
       <div className="flex flex-row flex-wrap gap-10 items-center justify-center">
         {games.map((g) => (
           <Card key={g.year}>
-            <CardContent>
-              <Link to="/games/$id" params={{ id: g.year.toString() }}>
-                <img
-                  src="https://placehold.co/400"
-                  className="rounded-md mb-2"
-                />
-              </Link>
+            <CardContent className="">
+              <div className="bg-white flex h-64 w-64 overflow-hidden items-center justify-center p-4 rounded-md">
+                <Link to="/games/$id" params={{ id: g.year.toString() }} className="">
+                  <img
+                    src={g.imageUrl ?? "https://placehold.co/400"}
+                    className="h-full w-full object-cover"
+                  />
+                </Link>
+              </div>
             </CardContent>
             <CardHeader className="flex flex-col justify-start items-start w-full">
               <CardTitle className="flex flex-row items-start justify-between ">
                 <div className="text-2xl font-bold">
                   {g.year}:{" "}
-                  <span className="text-(--color-destructive)">{g.name}</span>
+                  <span className="text-(--color-destructive) line-clamp-1">{g.name}</span>
                 </div>
               </CardTitle>
               <CardDescription>
