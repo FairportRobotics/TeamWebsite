33c1cbc9ec416f8f68bcdba337e6d0b3d49803f6
John Hurrell <jhurrell@gmail.com>
2026-05-11 15:42:38 -0400

    Added information link. Changed display of dates to be nicer.

--- src/components/test-form.tsx
+++ src/components/test-form.tsx
@@ -28,6 +28,7 @@ interface Calendar {
   location: string;
   visibleTo: Array<string>;
   dates: Array<CalendarDate>;
+  informationLink?: string | undefined;
   signupLink?: string | undefined;
   signupLinkVisibleTo: Array<string>;
 }
@@ -38,6 +39,8 @@ const defaultCalendar: Calendar = {
   location: "",
   visibleTo: [Roles.Everyone],
   dates: [] as CalendarDate[],
+  informationLink: undefined,
+  signupLink: undefined,
   signupLinkVisibleTo: [Roles.Student, Roles.Mentor],
 };
 
@@ -54,9 +57,8 @@ const calendarSchema = z
     visibleTo: z
       .array(z.enum(VisibleToOptions))
       .min(1, "At least one visibility option must be selected"),
-
     dates: z.array(dateSchema).min(1, "At least one date range is required"),
-
+    informationLink: z.url().optional().or(z.literal("")),
     signupLink: z.url().optional().or(z.literal("")),
     signupLinkVisibleTo: z
       .array(z.enum(VisibleToOptions))
@@ -82,6 +84,7 @@ const calendarSchema = z
   );
 
 export const TestForm = () => {
+  const [showInformation, setShowInformation] = useState<boolean>(false);
   const [showSignup, setShowSignup] = useState<boolean>(false);
 
   const form = useForm({
@@ -99,6 +102,13 @@ export const TestForm = () => {
     //console.log(form.state.values);
     console.log("form.state.values:", JSON.stringify(form.state.values, null, 2));
     console.log("form.state.errorMap:", JSON.stringify(form.state.errorMap, null, 2));
+  }
+
+  function handleToggleHasInformation(checked: boolean) {
+    setShowInformation(checked);
+    if (!checked) {
+      form.state.values.informationLink = "";
+    }
   }
 
   function handleToggleHasSignup(checked: boolean) {
@@ -232,22 +242,29 @@ export const TestForm = () => {
                 </ul>
               </div>
               <div className="gap-2 flex flex-col mt-4">
-                {field.state.value.map((date, index) => (
-                  <div
-                    key={index}
-                    className="flex flex-row items-center hover:bg-slate-800 rounded-md p-1"
-                  >
-                    <Button
-                      onClick={() => handleRemoveDate(index)}
-                      variant="destructive"
-                      className="hover:cursor-pointer mr-3 w-8 h-8"
+                {field.state.value
+                  .sort((a, b) => a.startAt.toISOString().localeCompare(b.endAt.toISOString()))
+                  .map((date, index) => (
+                    <div
+                      key={index}
+                      className="flex flex-row items-center hover:bg-slate-800 rounded-md p-1"
                     >
-                      <Trash2 className="" />
-                    </Button>
-                    {date.startAt.toLocaleDateString()} from {date.startAt.toLocaleTimeString()} to{" "}
-                    {date.endAt.toLocaleTimeString()}
-                  </div>
-                ))}
+                      <Button
+                        onClick={() => handleRemoveDate(index)}
+                        variant="destructive"
+                        className="hover:cursor-pointer mr-3 w-8 h-8"
+                      >
+                        <Trash2 className="" />
+                      </Button>
+                      <div className="flex flex-row gap-3">
+                        <span>{date.startAt.toLocaleDateString()}</span>
+                        <span>from</span>
+                        <span>{date.startAt.toLocaleTimeString()}</span>
+                        <span>to</span>
+                        <span>{date.endAt.toLocaleTimeString()}</span>
+                      </div>
+                    </div>
+                  ))}
               </div>
             </div>
           )}
@@ -294,8 +311,53 @@ export const TestForm = () => {
           )}
         />
 
+        {/* Information Options */}
+        <div className="flex flex-col mt-6">
+          <div className="flex flex-row">
+            <Checkbox
+              id="has-signup"
+              checked={showInformation}
+              onCheckedChange={(checked) => handleToggleHasInformation(checked ? true : false)}
+              className="data-[state=checked]:bg-blue-600 w-6 h-6"
+            />
+            <Label htmlFor="has-signup" className="ml-2 cursor-pointer font-bold text-lg">
+              Include a link for more information?
+            </Label>
+          </div>
+          {showInformation && (
+            <div className="space-y-4 pl-6 mt-3">
+              <form.Field
+                name="informationLink"
+                children={(field) => (
+                  <div>
+                    <Label className="mb-3 font-bold text-lg">Information URL:</Label>
+                    <Input
+                      name={field.name}
+                      value={field.state.value ?? ""}
+                      onBlur={field.handleBlur}
+                      placeholder="Enter information URL"
+                      autoComplete="off"
+                      type="text"
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                    <ul className="text-red-600 list-disc list-inside">
+                      {field.state.meta.errors.map((e) => {
+                        return (
+                          <li className="" key={e?.message}>
+                            {e?.message}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                )}
+              />
+            </div>
+          )}
+        </div>
+
         {/* Signup Options */}
-        <div className="flex flex-col my-6">
+        <div className="flex flex-col mt-6">
           <div className="flex flex-row">
             <Checkbox
               id="has-signup"
@@ -304,7 +366,7 @@ export const TestForm = () => {
               className="data-[state=checked]:bg-blue-600 w-6 h-6"
             />
             <Label htmlFor="has-signup" className="ml-2 cursor-pointer font-bold text-lg">
-              Has a Signup Form
+              Include a link to a signup form?
             </Label>
           </div>
 
@@ -320,7 +382,7 @@ export const TestForm = () => {
                       name={field.name}
                       value={field.state.value ?? ""}
                       onBlur={field.handleBlur}
-                      placeholder="Enter link"
+                      placeholder="Enter signup URL"
                       autoComplete="off"
                       type="text"
                       onChange={(e) => field.handleChange(e.target.value)}
