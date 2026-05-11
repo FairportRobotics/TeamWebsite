10262872ceeb9fc0865432cd3e6b9a1fca5bb04e
John Hurrell <jhurrell@gmail.com>
2026-05-11 08:34:51 -0400

    Working with Textarea which preserves carriage returns.

--- src/components/test-form.tsx
+++ src/components/test-form.tsx
@@ -3,6 +3,7 @@ import { useForm } from "@tanstack/react-form";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
 
 const VisibleToOptions = [Roles.Everyone, Roles.Student, Roles.Mentor, Roles.Parent] as const;
 
@@ -29,59 +30,85 @@ export const TestForm = () => {
         form.handleSubmit();
       }}
     >
-      <form.Field
-        name="title"
-        validators={{
-          // We can choose between form-wide and field-specific validators
-          onChange: ({ value }) => (value.trim().length === 0 ? undefined : "Title is required"),
-        }}
-        children={(field) => (
-          <>
-            <Label>Title</Label>
-            <Input
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              type="text"
-              onChange={(e) => field.handleChange(e.target.value)}
-            />
-            {!field.state.meta.isValid && <em>{field.state.meta.errors.join(",")}</em>}
-          </>
-        )}
-      />
-      <form.Field
-        name="visibleTo"
-        validators={{
-          // We can choose between form-wide and field-specific validators
-          onChange: ({ value }) =>
-            value.length === 0 ? "At least one visibility option must be selected" : undefined,
-        }}
-        children={(field) => (
-          <>
-            <Label>Visible To</Label>
-            {VisibleToOptions.map((option) => (
-              <div key={option}>
-                <label>
-                  <input
-                    type="checkbox"
-                    name={field.name}
-                    value={option}
-                    checked={field.state.value.includes(option)}
-                    onChange={(e) => {
-                      const newValue = e.target.checked
-                        ? [...field.state.value, option]
-                        : field.state.value.filter((v) => v !== option);
-                      field.handleChange(newValue);
-                    }}
-                  />
-                  {option}
-                </label>
-              </div>
-            ))}
-            {!field.state.meta.isValid && <em>{field.state.meta.errors.join(",")}</em>}
-          </>
-        )}
-      />
+      <div className="flex flex-col gap-6">
+        {/* Title */}
+        <form.Field
+          name="title"
+          validators={{
+            onBlur: ({ value }) => (value.trim() === "" ? "Title is required" : undefined),
+          }}
+          children={(field) => (
+            <div>
+              <Label>Title</Label>
+              <Input
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                type="text"
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+              {!field.state.meta.isValid && <em>{field.state.meta.errors.join(",")}</em>}
+            </div>
+          )}
+        />
+
+        {/* Description */}
+        <form.Field
+          name="description"
+          validators={{
+            onBlur: ({ value }) => (value.trim() === "" ? "Description is required" : undefined),
+          }}
+          children={(field) => (
+            <div>
+              <Label>Description</Label>
+              <Textarea
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                rows={10}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+              {!field.state.meta.isValid && <em>{field.state.meta.errors.join(",")}</em>}
+            </div>
+          )}
+        />
+
+        {/* Visible To */}
+        <form.Field
+          name="visibleTo"
+          validators={{
+            // We can choose between form-wide and field-specific validators
+            onBlur: ({ value }) =>
+              value.length === 0 ? "At least one visibility option must be selected" : undefined,
+          }}
+          children={(field) => (
+            <div>
+              <Label>Visible To</Label>
+              {VisibleToOptions.map((option) => (
+                <div key={option}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name={field.name}
+                      value={option}
+                      checked={field.state.value.includes(option)}
+                      onChange={(e) => {
+                        const newValue = e.target.checked
+                          ? [...field.state.value, option]
+                          : field.state.value.filter((v) => v !== option);
+                        field.handleChange(newValue);
+                      }}
+                    />
+                    {option}
+                  </label>
+                </div>
+              ))}
+              {!field.state.meta.isValid && <em>{field.state.meta.errors.join(",")}</em>}
+            </div>
+          )}
+        />
+      </div>
+
       <div className="mt-8">
         <Button type="submit">Submit</Button>
       </div>
