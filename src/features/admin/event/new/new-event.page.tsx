1144783026ab3ca6685cc1102e0b7eb5d7bad966
John Hurrell <jhurrell@gmail.com>
2026-06-11 08:13:01 -0400

    Renamed some files.

--- src/features/admin/event/new/new-event.page.tsx
+++ src/features/admin/event/new/new-event.page.tsx
@@ -1,7 +1,7 @@
 import { BackTo } from "@/components/site/BackTo";
 import { PageHeader, PageTitle } from "@/components/site/PageHeader";
 import type { VisibleEnumType } from "@/db/schema";
-import { CommonEventForm, type CalendarFormValues } from "@/features/admin/event/_common/common-event-form";
+import { CommonEventForm, type CalendarFormValues } from "@/features/admin/event/_common/common-event.form";
 import { createEventSchema } from "@/features/admin/event/schemas/create-event.schema";
 import { createEventFn } from "@/server/functions/calendar/createEvent";
 import { toast } from "sonner";
